7561dd5d8a8aac44196060258049c621fd8b3302
David Jones <kingdavidjones@Davids-MacBook-Pro.local>
2020-10-18 16:25:27 -0600

    pagination

---
+++ src/Components/RestaurantData.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function RestaurantData({ restaurantData, loading }) {
+  if (loading) {
+    console.log("loading");
+  }
+
+  return (
+    <>
+      {restaurantData.map((restaurant) => (
+        <tr key={restaurant.id}>
+          <td>{restaurant.name}</td>
+          <td>{restaurant.city}</td>
+          <td>{restaurant.state}</td>
+          <td>{restaurant.telephone}</td>
+          <td>{restaurant.genre}</td>
+        </tr>
+      ))}
+    </>
+  );
+}
+
+export default RestaurantData;
