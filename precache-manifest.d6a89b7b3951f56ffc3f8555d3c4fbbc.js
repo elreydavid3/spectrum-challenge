52db930c0cc3dc68abe97b029376d1512f4a1c75
David Jones <kingdavidjones@Davids-MacBook-Pro.local>
2020-10-19 15:57:35 -0600

    Updates

---
+++ precache-manifest.d6a89b7b3951f56ffc3f8555d3c4fbbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94cd0e64992af50dc5c7932f53b10035",
+    "url": "/spectrum-challenge/index.html"
+  },
+  {
+    "revision": "45d81245e30c3d10c7a2",
+    "url": "/spectrum-challenge/static/css/main.9314add7.chunk.css"
+  },
+  {
+    "revision": "53af5d773ca05c10c7c9",
+    "url": "/spectrum-challenge/static/js/2.a975a80d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/spectrum-challenge/static/js/2.a975a80d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d81245e30c3d10c7a2",
+    "url": "/spectrum-challenge/static/js/main.7cb235aa.chunk.js"
+  },
+  {
+    "revision": "e61cee988ef95aa42b37",
+    "url": "/spectrum-challenge/static/js/runtime-main.0dc734aa.js"
+  }
+]);
